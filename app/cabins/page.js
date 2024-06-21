02416aa35b2887cf4a3a354285689776f9af6eb4
Oliver <oliver.gorgiev11@gmail.com>
2024-06-21 18:16:05 +0200

    adding search functionality & using all the search hooks from nextjs

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -4,6 +4,7 @@ import { getCabins } from "../_lib/data-service";
 import CabinList from "../_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: "Cabins",
@@ -11,7 +12,10 @@ export const metadata = {
 
 export const revalidate = 3600;
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  console.log(searchParams);
+  const filter = searchParams?.capacity ?? "all";
+  console.log(filter);
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -25,8 +29,11 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
